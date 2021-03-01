523ede5c0205e17f975dac210fcae583a7e748eb
Jordan Thomas <jordan@newrig.hsd1.tn.comcast.net>
2021-02-28 19:44:59 -0600

    begin to add optional parts, comments, subjects, etc.  Currently only filename is implemented

--- DesktopThread.py
+++ DesktopThread.py
@@ -14,22 +14,36 @@ import argparse
 import time
 import requests
 import json
-from pprint import pprint
-from datetime import datetime as dt
 
 captchatime = 0
 config = configparser.ConfigParser()
 config.read("desktopthread.cfg")
     
 parser = argparse.ArgumentParser(description='Post desktop on /g/')
-parser.add_argument("-G", "--gold", help="sign into 4chan gold", action="store_true")
+parser.add_argument("-G", "-g", "--gold",              help="sign into 4chan gold", action="store_true")
+parser.add_argument("-F", "-f", "--file",    type=str, help="absolute path to file for upload, overrides config file")
+parser.add_argument("-N", "-n", "--name",    type=str, help="Name to use when posting")
+parser.add_argument("-S", "-s", "--sub",     type=str, help="Subject to use if a new thread is made")
+parser.add_argument("-C", "-c", "--comment", type=str, help="Comment to use when posting")
 
 args = parser.parse_args()
 if args.gold:
-    print ("gold account detected")
+    print("gold account usage requested")
 else:
     #how long to give poster to solve captcha
     captchatime = 20
+
+#find our file name
+filename = ""
+if args.file:
+    filename = args.file
+else:
+    if config['top']['staticfile'].lower == "yes":
+            filename = config['top']['staticpath']
+    else:
+        os.system(config['top']['scrot-cmd'])
+        filename = config['top']['scrot-file']
+
 
 if(config['system']['webdriver'].lower() == "chromedriver"):
     browser = webdriver.Chrome()
@@ -54,10 +68,10 @@ if args.gold:
         element_present = EC.presence_of_element_located((By.ID, 'logout-form'))
         WebDriverWait(browser, timeout).until(element_present)
     except TimeoutException:
-        print =("Timed out waiting for page to load or login failed?")
+        print("Timed out waiting for page to load or login failed?")
 
 #browser.get('https://boards.4chan.org/g')
-print ("on /g/, about to look for desktop thread")
+print("on /g/, about to look for desktop thread")
 
 #going to use JSON API instead of this...
 #searchbox = browser.find_element_by_id("search-box")
@@ -65,6 +79,13 @@ print ("on /g/, about to look for desktop thread")
 #searchbox.send_keys(Keys.ENTER)
 
 
+
+#load up all posts on board using json api
+#currently we look for OP posts containing "desktop thread"
+#the first such thread we take as our thread - in the future
+#we could look at all threads that match and choose the best 
+#one based on current replies (is one about to die or was the
+# one we found started prematurely?)
 r = requests.get('https://a.4cdn.org/g/catalog.json')
 r = r.json()
 
@@ -79,8 +100,6 @@ found = False
 for threads in gen_chan():
     com = get_threads('com')
     no = get_threads('no')
-    #print(str(no) + ": " + com)
-
     if "desktop thread" in com.lower():
         print("desktop thread found at " + str(no))
         found = True
@@ -89,6 +108,7 @@ for threads in gen_chan():
 comment=""
 if found:    
     browser.get("https://boards.4channel.org/g/thread/" + str(no))
+    comment = "testing"
 else:
     print("no desktop thread found, going to /g/ main page")
     browser.get("https://boards.4channel.org/g/")
@@ -97,7 +117,11 @@ else:
 browser.find_element_by_id("togglePostFormLink").click()
 browser.find_element_by_name("com").send_keys(comment)
 
-filename = config['top']['scrot-file']
+
+
+print("going to upload" + filename)
 uploader = browser.find_element_by_name("upfile")
 uploader.send_keys(filename)
+
+#IMPORTANT - commented to avoid posting while testing
 browser.find_element_by_name("post").submit()
